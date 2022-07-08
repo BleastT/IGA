899890cd9868deb5ee58a19a30c3a04d1e8a7e7d
Yassine A <AYassinee2007@gmail.com>
2022-07-07 21:53:11 -0400

    fixed a small bug

--- Scripts/index.js
+++ Scripts/index.js
@@ -5,8 +5,8 @@ var redirect = document.getElementById("redirect");
 window.onload = () => {
     if(sessionStorage.getItem("userId") === null || sessionStorage.getItem("userId") === "disconnected")
     {     
-        redirect.insertAdjacentHTML("beforeend", `<a href="./Pages/Login.html">Se connecter</a>`);
-        redirect.insertAdjacentHTML("beforeend", `<a href="./Pages/createAccount.html">S'inscrire</a>`);  
+        redirect.insertAdjacentHTML("beforeend", `<a href="./Pages/Login.html" class="btn">Se connecter</a>`);
+        redirect.insertAdjacentHTML("beforeend", `<a href="./Pages/createAccount.html" class="btn">S'inscrire</a>`);  
     }
     else
     {
